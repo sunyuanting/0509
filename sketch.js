e69b6ca15fce95bb37fad0aaa5056f1ade03b7fa
yuan <115965720+sunyuanting@users.noreply.github.com>
2025-05-09 10:54:59 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,15 @@
 let video;
+let poseNet;
+let pose;
+let skeleton;
 let hands;
 
 function setup() {
   createCanvas(640, 480);
   video = createCapture(VIDEO);
-  video.size(640, 480);
   video.hide();
+  poseNet = ml5.poseNet(video, modelLoaded); // 呼叫PoseNet函數
+  poseNet.on('pose', gotPoses);
 
   let handpose = ml5.handpose(video, () => console.log('Handpose ready'));
   handpose.on('hand', (results) => {
@@ -13,15 +17,39 @@ function setup() {
   });
 }
 
+function gotPoses(poses) {
+  if (poses.length > 0) {
+    pose = poses[0].pose; // 把抓到的幾個點放置到pose變數內
+    skeleton = poses[0].skeleton; // 把骨架的點放到skeleton變數內
+  }
+}
+
+function modelLoaded() { // 顯示PoseNet模型已準備就緒
+  console.log('poseNet ready');
+}
+
 function draw() {
-  background(220);
+  background(0);
   translate(video.width, 0); // 因為攝影機顯示的是反像的畫面，需要透過這兩條指令來做反轉
   scale(-1, 1);             // 因為攝影機顯示的是反像的畫面，需要透過這兩條指令來做反轉
-  image(video, 0, 0, 640, 480);
+  image(video, 0, 0); // 顯示你的畫面在螢幕上
+
+  if (pose) {
+    let eyeR = pose.rightEye;  // 抓到右眼資訊，放到eyeR
+    let eyeL = pose.leftEye;   // 抓到左眼資訊，放到eyeL
+    let d = dist(eyeR.x, eyeR.y, eyeL.x, eyeL.y); // 算出左右眼的距離，當作鼻子顯示圓的直徑
+    fill(255, 0, 0);
+    ellipse(pose.nose.x, pose.nose.y, d); // 畫出鼻子的圓
+    fill(0, 0, 255);
+    ellipse(pose.rightWrist.x, pose.rightWrist.y, 62); // 畫出右手腕圓圈
+    ellipse(pose.leftWrist.x, pose.leftWrist.y, 62); // 畫出左手腕圓圈
+    drawKeypoints();
+    drawSkeleton();
+  }
 
   if (hands) {
     for (let hand of hands) {
-      for (let i = 0; i <= 4; i++) { // 只處理keypoints編號0到4
+      for (let i = 0; i <= 4; i++) { // 串接keypoints編號0到4
         let x = hand.landmarks[i][0];
         let y = hand.landmarks[i][1];
         fill(255, 0, 0);
@@ -30,7 +58,7 @@ function draw() {
         textSize(12);
         text(i, x + 5, y - 5); // 標記keypoint編號
 
-        if (i > 0) { // 串接keypoints 0到4
+        if (i > 0) { // 串接keypoints
           let prevX = hand.landmarks[i - 1][0];
           let prevY = hand.landmarks[i - 1][1];
           stroke(0, 255, 0);
@@ -46,9 +74,9 @@ function draw() {
         ellipse(x, y, 10, 10);
         fill(255);
         textSize(12);
-        text(i, x + 5, y - 5); // 標記keypoint編號
+        text(i, x + 5, y - 5);
 
-        if (i > 5) { // 串接keypoints
+        if (i > 5) {
           let prevX = hand.landmarks[i - 1][0];
           let prevY = hand.landmarks[i - 1][1];
           stroke(255, 0, 0);
@@ -64,9 +92,9 @@ function draw() {
         ellipse(x, y, 10, 10);
         fill(255);
         textSize(12);
-        text(i, x + 5, y - 5); // 標記keypoint編號
+        text(i, x + 5, y - 5);
 
-        if (i > 9) { // 串接keypoints
+        if (i > 9) {
           let prevX = hand.landmarks[i - 1][0];
           let prevY = hand.landmarks[i - 1][1];
           stroke(255, 255, 0);
@@ -82,9 +110,9 @@ function draw() {
         ellipse(x, y, 10, 10);
         fill(255);
         textSize(12);
-        text(i, x + 5, y - 5); // 標記keypoint編號
+        text(i, x + 5, y - 5);
 
-        if (i > 13) { // 串接keypoints
+        if (i > 13) {
           let prevX = hand.landmarks[i - 1][0];
           let prevY = hand.landmarks[i - 1][1];
           stroke(128, 0, 128);
@@ -100,9 +128,9 @@ function draw() {
         ellipse(x, y, 10, 10);
         fill(255);
         textSize(12);
-        text(i, x + 5, y - 5); // 標記keypoint編號
+        text(i, x + 5, y - 5);
 
-        if (i > 17) { // 串接keypoints
+        if (i > 17) {
           let prevX = hand.landmarks[i - 1][0];
           let prevY = hand.landmarks[i - 1][1];
           stroke(0, 128, 128);
@@ -111,5 +139,24 @@ function draw() {
         }
       }
     }
+  }
+}
+
+function drawKeypoints() {  
+  for (let i = 0; i < pose.keypoints.length; i++) {
+    let x = pose.keypoints[i].position.x; // 找出每一個點的x座標
+    let y = pose.keypoints[i].position.y; // 找出每一個點的y座標
+    fill(0, 255, 0);
+    ellipse(x, y, 16, 16);
+  }
+}
+
+function drawSkeleton() {
+  for (let i = 0; i < skeleton.length; i++) {
+    let a = skeleton[i][0];
+    let b = skeleton[i][1];
+    strokeWeight(2);
+    stroke(255);
+    line(a.position.x, a.position.y, b.position.x, b.position.y);
   }
 }
